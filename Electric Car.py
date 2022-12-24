aafb0d4f5fca7535024c2837b11d282c676363fb
Ahmad Mushtaq <ahmadbinmushtaq99@gmail.com>
2022-12-24 12:33:32 +0500

    Velocity function working

--- Electric Car.py
+++ Electric Car.py
@@ -2,90 +2,39 @@ import time
 import serial
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import numpy as np
 
+def animate(i, dataList, ser):
+    ser.write(b'g')                                     # Transmit the char 'g' to receive the Arduino data point
+    arduinoData_string = ser.readline().decode('ascii') # Decode receive Arduino data as a formatted string
+    #print(i)                                           # 'i' is a incrementing variable based upon frames = x argument
 
-class AnimationPlot:
+    try:
+        arduinoData_float = float(arduinoData_string)   # Convert to float
+        dataList.append(arduinoData_float)              # Add to the list holding the fixed number of points to animate
 
-    def animate(self, i, dataList, ser):
-        ser.write(b'x')  # Transmit the char 'g' to receive the Arduino data point
-        arduinoData_string = ser.readline().decode('ascii')  # Decode receive Arduino data as a formatted string
-        #print(i)                                           # 'i' is a incrementing variable based upon frames = x argument
+    except:                                             # Pass if data point is bad                               
+        pass
 
-        try:
-            arduinoData_float = float(arduinoData_string)  # Convert to float
-            dataList.append(arduinoData_float)  # Add to the list holding the fixed number of points to animate
+    dataList = dataList[-50:]                           # Fix the list size so that the animation plot 'window' is x number of points
+    
+    ax.clear()                                          # Clear last data frame
+    ax.plot(dataList)                                   # Plot new data frame
+    
+    ax.set_ylim([0, 50])                              # Set Y axis limit of plot
+    ax.set_title("Arduino Data")                        # Set title of figure
+    ax.set_ylabel("Value")                              # Set title of y axis 
 
-        except:  # Pass if data point is bad
-            pass
+dataList = []                                           # Create empty list variable for later use
+                                                        
+fig = plt.figure()                                      # Create Matplotlib plots fig is the 'higher level' plot window
+ax = fig.add_subplot(111)                               # Add subplot to main fig window
 
-        dataList = dataList[-500000:]  # Fix the list size so that the animation plot 'window' is x number of points
+ser = serial.Serial("COM5", 9600)                       # Establish Serial object with COM port and BAUD rate to match Arduino Port/rate
+time.sleep(2)                                           # Time delay for Arduino Serial initialization 
 
-        ax.clear()  # Clear last data frame
+                                                        # Matplotlib Animation Fuction that takes takes care of real time plot.
+                                                        # Note that 'fargs' parameter is where we pass in our dataList and Serial object. 
+ani = animation.FuncAnimation(fig, animate, frames=100, fargs=(dataList, ser), interval=100) 
 
-        self.getPlotFormat()
-        ax.plot(dataList)  # Plot new data frame
-
-    def getPlotFormat(self):
-        ax.set_ylim([-1200000, 1200000])  # Set Y axis limit of plot
-        ax.set_title("Real Time Arduino Data")  # Set title of figure
-        ax.set_ylabel("Value")  # Set title of y axis
-
-
-dataList = []  # Create empty list variable for later use
-
-#fig, axs = plt.subplots(4)
-
-
-fig = plt.figure()  # Create Matplotlib plots fig is the 'higher level' plot window
-ax = fig.add_subplot(221)  # Add subplot to main fig window
-#plt.scatter(x,y)
-#ax = fig.add_subplot(412)
-
-
-
-
-
-
-
-#fig.add_subplot(221)   #top left
-fig.add_subplot(222).set_title('2nd graph')  #top right
-plt.xlabel("Average Pulse")
-plt.ylabel("Calorie Burnage")
-xpoints = np.array([0,12000])
-ypoints = np.array([-1000, 1000])
-
-
-
-fig.add_subplot(223).set_title('2nd graph')  #bottom left
-plt.xlabel("Average Pulse")
-plt.ylabel("Calorie Burnage")
-
-
-fig.add_subplot(224).set_title('2nd graph')   #bottom right
-plt.xlabel("Average Pulse")
-plt.ylabel("Calorie Burnage")
-
-
-fig.tight_layout()
-plt.xlabel("Currrent")
-plt.ylabel("Power")
-
-
-
-
-realTimePlot = AnimationPlot()
-
-ser = serial.Serial("COM5", 9600)  # Establish Serial object with COM port and BAUD rate to match Arduino Port/rate
-time.sleep(2)  # Time delay for Arduino Serial initialization
-
-# Matplotlib Animation Fuction that takes takes care of real time plot.
-# Note that 'fargs' parameter is where we pass in our dataList and Serial object.
-ani = animation.FuncAnimation(fig, realTimePlot.animate, frames=100, fargs=(dataList, ser), interval=100)
-
-plt.show()  # Keep Matplotlib plot persistent on screen until it is closed
-ser.close()
-
-
-
-
+plt.show()                                              # Keep Matplotlib plot persistent on screen until it is closed
+ser.close()                                             # Close Serial connection when plot is closed
